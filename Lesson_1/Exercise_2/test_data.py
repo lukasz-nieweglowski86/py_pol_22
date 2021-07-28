d9115fa0f757b56a3b0c9d7ebb30ee11eb82d7f7
Łukasz Niewęgłowski <lukasz.nieweglowski86@gmail.com>
2021-07-28 20:38:09 +0200

    refactor

---
+++ Lesson_1/Exercise_2/test_data.py
@@ -0,0 +1,11 @@
+# homepage_url
+homepage_url = 'http://localhost/addressbook/addressbook/'
+
+# admin_data
+admin_login = 'admin'
+admin_password = 'secret'
+
+# group_data
+group_name = 'Test group'
+group_header = 'Test group header'
+group_footer = 'Test group footer'
