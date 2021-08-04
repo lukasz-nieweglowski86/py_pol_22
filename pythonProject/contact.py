ac39e7c3451686600b7ecd0ec3eb421c2f6953ee
Łukasz Niewęgłowski <lukasz.nieweglowski86@gmail.com>
2021-08-04 20:19:10 +0200

    refactor

---
+++ pythonProject/contact.py
@@ -0,0 +1,29 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company,
+                 address1, home_phonenumber, mobile_phonenumber, work_phonenumber,
+                 fax, email1, email2, email3, homepage, bday, bmonth, byear, aday,
+                 amonth, ayear, address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address1 = address1
+        self.home_phonenumber = home_phonenumber
+        self.mobile_phonenumber = mobile_phonenumber
+        self.work_phonenumber = work_phonenumber
+        self.fax = fax
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
