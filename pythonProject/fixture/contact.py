85663157c69e10e45432be01c17f008abecab966
Łukasz Niewęgłowski <lukasz.nieweglowski86@gmail.com>
2021-08-11 23:04:18 +0200

    - created fixture for all tests in conftest.py - created test with contact deletion

--- pythonProject/fixture/contact.py
+++ pythonProject/fixture/contact.py
@@ -92,6 +92,13 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.return_to_home_page()
 
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # select first contact
+        wd.find_element_by_xpath("//img[@title='Edit']").click()
+        # submit deletion
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+
     def return_to_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
