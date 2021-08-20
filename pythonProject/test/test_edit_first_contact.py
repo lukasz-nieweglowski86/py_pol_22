96533feccda3f717d857d13468f53121a1caafb2
Łukasz Niewęgłowski <lukasz.nieweglowski86@gmail.com>
2021-08-20 22:23:05 +0200

    edition and deletion tests modified

--- pythonProject/test/test_edit_first_contact.py
+++ pythonProject/test/test_edit_first_contact.py
@@ -2,6 +2,15 @@ from model.contact import Contact
 
 
 def test_edit_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.add(Contact(firstname="Test_firstname", middlename="Test_middlename",
+                                lastname="Test_lastname", nickname="Test_nickname", title="Test_title",
+                                company="Test_company", address1="Test_address1", home_phonenumber="123123123",
+                                mobile_phonenumber="321321321", work_phonenumber="135135135", fax="123456789",
+                                email1="test_email1@test.com", email2="test_email2@test.com",
+                                email3="test_email3@test.com", homepage="www.google.com", bday="11",
+                                bmonth="March", byear="1986", aday="7", amonth="July", ayear="2020",
+                                address2="Test_address2", phone2="909808707", notes="Lorem ipsum..."))
     app.contact.edit_first_contact(Contact(firstname="edited", middlename="edited",
                                            lastname="edited", nickname="edited", title="edited",
                                            company="edited", address1="edited",
--- pythonProject/test/test_edit_first_group.py
+++ pythonProject/test/test_edit_first_group.py
@@ -2,8 +2,12 @@ from model.group import Group
 
 
 def test_edit_first_group_name(app):
-    app.group.edit_first_group(Group(name="edited"))
+    if app.group.count() == 0:
+        app.group.add(Group(name="added group", header_name="test header", footer_name="test footer"))
+    app.group.edit_first_group(Group(name="name edited"))
 
 
 def test_edit_first_group_header(app):
-    app.group.edit_first_group(Group(header_name="edited"))
+    if app.group.count() == 0:
+        app.group.add(Group(name="added group", header_name="test header", footer_name="test footer"))
+    app.group.edit_first_group(Group(header_name="header edited"))
