ee3fd718bdb85851d65d54f5b03bc0db06076714
Łukasz Niewęgłowski <lukasz.nieweglowski86@gmail.com>
2021-09-08 21:03:10 +0200

    sending parameters from command line added

--- pythonProject/fixture/application.py
+++ pythonProject/fixture/application.py
@@ -6,11 +6,19 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,7 +31,7 @@ class Application:
         wd = self.wd
         if not (wd.current_url.endswith("localhost/addressbook/") and
                 len(wd.find_elements_by_name("searchstring")) > 0):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
